7a5664c5fb7b752a3f43dafde6e5f27462fdea42
theodore <njuzheqin@gmail.com>
2016-10-19 00:38:27 -0400

    more test on the malloc_alloc allocator:

--- src/m_alloc.h
+++ src/m_alloc.h
@@ -10,6 +10,7 @@
 #include <climits>  //for UINT_MAX
 
 namespace my_stl {
+    class __malloc_alloc;
 
     //this is a naive implementation of the allocator function
     //just wrap the new and delete under the allocate and deallocate method
@@ -81,9 +82,63 @@ namespace my_stl {
     };
 
 
+
     //testing the naive allocator we can see that it is not doing very well, in SGI implementation
     //the allocator is divided into two level: first level is using malloc and free to allocate 
     //and manage memory, the second level(sub-allocator) is using complex memory pool
+    //--------------------------simple allocator, generalized class--------------------------
+    template <typename _Tp, typename Alloc = __malloc_alloc>
+    class my_simple_alloc {
+        //this allocator provide the STL standrad interface of allocator, in the SGI implementation
+        //simple_alloc is used to combine the first level and second level allocator
+        //implemented 10/18 using only __malloc_alloc to test the performace
+        //by STL standard, these are the necessary interface for allocator
+        public:
+            //typename can be accessed using scope operator, but when used, need to 
+            //specifiy "typename"
+            typedef _Tp value_type;
+            typedef _Tp* pointer;
+            typedef const _Tp* const_pointer;
+            typedef _Tp& reference;
+            typedef const _Tp& const_reference;
+            typedef size_t size_type;
+            typedef ptrdiff_t diference_type;
+
+            //nested class template with only one data member
+            template <class _Tp1> 
+            struct rebind{
+                typedef my_simple_alloc<_Tp1> other;
+            };
+            /*                 //generic copy constructor */
+            /*                 template<class _Tp1> allocator(const allocator<_Tp1>&); */
+
+            //get back the address of one item
+            inline pointer address(reference tp) {return &tp;}
+
+            //get back the const address of one item
+            inline const pointer address(const_reference tp) const{return &tp;}
+
+            //allocator, second argument is just to enhance locality
+            inline pointer allocate(size_type n, const void* = 0) {
+                return (pointer) Alloc::allocate(n * sizeof(_Tp));
+            }
+
+            //return all the memory allocated, n has to be the same as we run allocate
+            inline void deallocate(pointer p, size_type n) {
+                Alloc::deallocate(p, n * sizeof(_Tp));
+            }
+
+            //the max volumn
+            size_type max_size() const {
+                return UINT_MAX / sizeof (_Tp);
+            }
+
+            //construct x for the pointer, use placement new
+            inline void construct(pointer p, const_reference x){new (p) _Tp(x);};
+
+            //destroy this object
+            inline void destroy(pointer p) {p -> ~_Tp();};
+    };
     //
     //--------------------------first level allocator: malloc_alloc---------------------------
     class __malloc_alloc {
--- test/m_alloc_test.cpp
+++ test/m_alloc_test.cpp
@@ -7,25 +7,27 @@
 
 #define TEST_EPOCH 1000000
 namespace my_stl {
-    void testNaiveAllocWithVector () {
+
+    template<typename allocator>
+    void testMyAllocWithVectorOperations () {
         //create a size 0 vector
-        std::vector<int, naive_allocator<int>> test_vec;
+        std::vector<int, allocator> test_vec;
         auto tic = std::chrono::duration_cast<std::chrono::milliseconds> 
             (std::chrono::system_clock::now().time_since_epoch()).count();
         //test push_back
         for (size_t i = 0; i < TEST_EPOCH; ++i) {
-            assert(test_vec.size() == i);
+            //assert(test_vec.size() == i);
             test_vec.push_back(i);
         }
         size_t size = TEST_EPOCH;
         while (!test_vec.empty()) {
-            assert(test_vec.size() == size);
+            //assert(test_vec.size() == size);
             --size;
             test_vec.pop_back();
         }
         auto toc = std::chrono::duration_cast<std::chrono::milliseconds> 
             (std::chrono::system_clock::now().time_since_epoch()).count();
-        std::cout << "my simple implementation takes " << (toc - tic) << "ms " << std::endl;
+        std::cout << "my implementation takes " << (toc - tic) << "ms " << std::endl;
 
 
         //now test the sgi alloc
@@ -34,12 +36,12 @@ namespace my_stl {
             (std::chrono::system_clock::now().time_since_epoch()).count();
         //test push_back
         for (size_t i = 0; i < TEST_EPOCH; ++i) {
-            assert(sgi_vec.size() == i);
+            //assert(sgi_vec.size() == i);
             sgi_vec.push_back(i);
         }
         size = TEST_EPOCH;
         while (!test_vec.empty()) {
-            assert(sgi_vec.size() == size);
+            //assert(sgi_vec.size() == size);
             --size;
             sgi_vec.pop_back();
         }
@@ -58,9 +60,15 @@ int main() {
     std::cout << "not using malloc" << std::endl;
     #endif
     //test result, sgi does not define use malloc
-    my_stl::testNaiveAllocWithVector();
     //test on 10/18/16, pushing and poping 1M times
-    //simple implementation -- 126ms
+    std::cout << "test with vector push_back and pop_back: naive allocator" << std::endl;
+    my_stl::testMyAllocWithVectorOperations<my_stl::naive_allocator<int>>();
+    //naive implementation -- 126ms
     //sgi alloc implementation -- 28ms
+    std::cout << "test with vector push_back and pop_back: malloc_alloc: " << std::endl;
+    my_stl::testMyAllocWithVectorOperations<my_stl::my_simple_alloc<int>>();
+    //malloc_alloc implementation -- 79ms
+    //sgi alloc implementation -- 18ms
+
 }
 
