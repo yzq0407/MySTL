674dbe032fd510f450f5d67a4fb3e72583ecf820
Theodore Yang <njuzheqin@gmail.com>
2016-11-29 00:28:39 -0500

    add copy_backward func in algobase.h

--- src/m_algobase.h
+++ src/m_algobase.h
@@ -125,6 +125,137 @@ namespace my_stl {
         return result;
     }
 
+    template<typename _TYPE> 
+    inline _TYPE* __copy_t(const _TYPE* first, const _TYPE* last, _TYPE* result, __true_type) {
+        //so the pointer is pointing to the type with trivial operator=
+        //we only need to call memmove()
+        memmove(result, first, sizeof(_TYPE) * (last - first));
+        return result + (last - first);
+    }
+
+    template<typename _TYPE>
+    inline _TYPE* __copy_t(const _TYPE* first, const _TYPE* last, _TYPE* result, __false_type) {
+        //the operator= is not trivial, we need to call it one by one
+        //but at least, it is a random access iterator
+        for (ptrdiff_t distance = last - first; distance > 0; --distance, ++first, ++result) {
+            *result = *first;
+        }
+        return result;
+    }
+    //-----------------------------------------------------------------------------------
+    //************************* copy_backward implementation***************************
+    //__________________________________________________________________________________
+    
+    //-----------------specialized form, call memmove whenever possible----------------
+    inline char* copy_backward(char* first, char* last, char* result) {
+        ptrdiff_t distance = last - first;
+        memmove(result - distance, first, distance);
+        return result - distance;
+    }
+
+    inline wchar_t* copy_backward(wchar_t* first, wchar_t* last, wchar_t* result) {
+        ptrdiff_t distance = last - first;
+        memmove(result - distance, first, sizeof(wchar_t) * distance);
+        return result - distance;
+    }
+
+    //-----------------the dispatch functor, which and be partially specialized into other form
+    template<typename _BI1, typename _BI2>
+    struct __copy_backward_dispatch {
+        _BI2 operator()(_BI1 first, _BI1 last, _BI2 result) {
+            return __copy_backward(first, last, result, 
+                    typename iterator_traits<_BI1>::iterator_categoty());
+        }
+    };
+
+    //-----------------pointer and const pointer type partial specialization
+    template<typename _TYPE>
+    struct __copy_backward_dispatch<_TYPE*, _TYPE*> {
+        _TYPE* operator() (_TYPE* first, _TYPE* last, _TYPE* result) {
+            typedef typename __type_traits<_TYPE>::has_trivial_assignment_operator _type;
+            return __copy_backward_t(first, last, result, _type());
+        }
+    };
+
+
+    template<typename _TYPE>
+    struct __copy_backward_dispatch<const _TYPE*, _TYPE*> {
+        _TYPE* operator() (const _TYPE* first, const _TYPE* last, _TYPE* result) {
+            typedef typename __type_traits<_TYPE>::has_trivial_assignment_operator _type;
+            return __copy_backward_t(first, last, result, _type());
+        }
+    };
+
+
+    //------------------four different functions to handle all the pointer types
+    template<typename _TYPE> 
+    inline _TYPE* __copy_backward_t(_TYPE* first, _TYPE* last, _TYPE* result, __true_type) {
+        //so the pointer is pointing to the type with trivial operator=
+        //we only need to call memmove()
+        ptrdiff_t distance = last - first;
+        memmove(result - distance, first,  sizeof(_TYPE) * distance);
+        return result - distance;
+    }
+
+    template<typename _TYPE>
+    inline _TYPE* __copy_backward_t(_TYPE* first, _TYPE* last, _TYPE* result, __false_type) {
+        //the operator= is not trivial, we need to call it one by one
+        //but at least, it is a random access iterator
+        for (ptrdiff_t distance = last - first; distance > 0; --distance) {
+            *--result = *--last;
+        }
+        return result;
+    }
+
+    template<typename _TYPE> 
+    inline _TYPE* __copy_backward_t(const _TYPE* first, const _TYPE* last, _TYPE* result, __true_type) {
+        //so the pointer is pointing to the type with trivial operator=
+        //we only need to call memmove()
+        ptrdiff_t distance = last - first;
+        memmove(result - distance, first,  sizeof(_TYPE) * distance);
+        return result - distance;
+    }
+
+    template<typename _TYPE>
+    inline _TYPE* __copy_backward_t(const _TYPE* first, const _TYPE* last, _TYPE* result, __false_type) {
+        //the operator= is not trivial, we need to call it one by one
+        //but at least, it is a random access iterator
+        for (ptrdiff_t distance = last - first; distance > 0; --distance) {
+            *--result = *--last;
+        }
+        return result;
+    }
+
+
+    //---------------------general iterator type, there are only two types
+    //------------------bidirectional,    or   random access
+    template<typename _BI1, typename _BI2>
+    inline _BI2 __copy_backward(_BI1 first, _BI1 last, _BI2 result, 
+            bidirectional_iterator_tag) {
+        while (first != last) {
+            --result = --last;
+        }
+        return result;
+    }
+
+    //random access iterator form
+    template<typename _BI1, typename _BI2>
+    inline _BI2 __copy_backward(_BI1 first, _BI1 last, _BI2 result, random_access_iterator_tag) {
+        //use distance to determine the for-loop times
+        for (typename iterator_traits<_BI1>::difference_type distance = last - first;
+                distance > 0; --distance) {
+            *--result = *--last;
+        }
+        return result;
+    }
+
+    
+    //-------------main copy_backward function, the input must be bidirectional------------
+    template<typename _BI1, typename _BI2>
+    inline _BI2 copy_backward(_BI1 first, _BI1 last, _BI2 result) {
+        return __copy_backward_dispatch<_BI1, _BI2>()(first, last, result);
+    }
+
     template <typename ForwardIterator, typename TYPE>
     void fill(ForwardIterator first, ForwardIterator last, const TYPE& val) {
         while (first != last) {
