d5e21fc9a2cf072957e488ef6c461920bbccb5e8
Theodore Yang <njuzheqin@gmail.com>
2016-12-05 01:29:11 -0500

    update test makefile and list test

--- test/m_list_test.cpp
+++ test/m_list_test.cpp
@@ -6,7 +6,12 @@
 TEST(ListTest, TestListConstruction) {
     my_stl::list<int> list;
     std::list<int> list2;
-
-    std::cout << *list.node << std::endl;
-
+    ASSERT_EQ(list.empty(), true) << "failed to create empty list";
+    ASSERT_EQ(list.size(), 0) << "empty list size is not 0";
+    list.push_back(1);
+    ASSERT_EQ(list.empty(), false) << "push back failed on empty test";
+    ASSERT_EQ(list.size(), 1) << "push back failed on size test";
+    list.push_back(2);
+    ASSERT_EQ(list.empty(), false);
+    ASSERT_EQ(list.size(), 2);
 }
