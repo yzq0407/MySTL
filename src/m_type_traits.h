0b33f8d62ef0786810902fb3a0f376a1644105ff
Theodore Yang <njuzheqin@gmail.com>
2016-12-19 00:07:11 -0500

    add more helper traits

--- src/m_type_traits.h
+++ src/m_type_traits.h
@@ -8,8 +8,52 @@
 namespace my_stl {
 //two types to identify true and false, we cannot use variables since we want compile time
 //identification of the types
-    struct __true_type {};
-    struct __false_type {};
+    struct __true_type {
+        enum {value = 1};
+    };
+    struct __false_type {
+        enum {value = 0};
+    };
+
+    //all the logical operation applied to true/false type
+    //-------------logical or-------------------------
+    template <typename... _B>
+    struct __or__ {
+        typedef __false_type type;
+        enum {value = 0};
+    };
+
+    template <typename... _B>
+    struct __or__ <__true_type, _B...> {
+        typedef __true_type type;
+        enum {value = 1};
+    };
+
+    template <typename... _B>
+    struct __or__ <__false_type, _B...> {
+        typedef typename __or__<_B...>::type type;
+        enum {value = __or__<_B...>::value};
+    };
+
+    //--------------logical and------------------------
+    template <typename... _B>
+    struct __and__ {
+        typedef __true_type type;
+        enum {value = 1};
+    };
+
+    template <typename... _B>
+    struct __and__ <__false_type, _B...> {
+        typedef __false_type type;
+        enum {value = 0};
+    };
+
+    template <typename... _B>
+    struct __and__ <__true_type, _B...> {
+        typedef typename __and__<_B...>::type type;
+        enum {value = __and__<_B...>::value};
+    };
+
 
     //the core part, exact the type information
     template <typename TYPE>
@@ -213,15 +257,15 @@ namespace my_stl {
 
     //----------is same type----------------
     template <typename, typename>
-    struct __are_same {
-        typedef __false_type __type;
-        enum {__value = 1};
+    struct __are_same{
+        typedef __false_type type;
+        enum {value = 0};
     };
 
     template <typename _Tp>
     struct __are_same<_Tp, _Tp> {
-        typedef __true_type __type;
-        enum {__value = 0};
+        typedef __true_type type;
+        enum {value = 1};
     };
 
 
@@ -241,10 +285,49 @@ namespace my_stl {
         typedef _Tp type;
     };
 
-
     //c++14, type aliasing allows us to do so
     template <typename _Tp>
     using remove_reference_t = typename remove_reference<_Tp>::type;
+
+    //------------remove const--------------
+    template <typename _Tp>
+    struct remove_const {
+        typedef _Tp type;
+    };
+
+    template <typename _Tp>
+    struct remove_const <const _Tp> {
+        typedef _Tp type;
+    };
+
+    //c++14
+    template <typename _Tp>
+    using remove_const_t = typename remove_const<_Tp>::type;
+
+    //----------remove volatile------------
+    template <typename _Tp>
+    struct remove_volatile {
+        typedef _Tp type;
+    };
+
+    template <typename _Tp>
+    struct remove_volatile <volatile _Tp> {
+        typedef _Tp type;
+    };
+
+    //c++14
+    template <typename _Tp>
+    using remove_volatile_t = typename remove_volatile<_Tp>::type;
+
+    //----------remove const and volatile----
+    template <typename _Tp>
+    struct remove_cv {
+        typedef typename remove_volatile<typename remove_const<_Tp>::type>::type type;
+    };
+
+    //c++14
+    template <typename _Tp>
+    using remove_cv_t = typename remove_cv<_Tp>::type;
 }
 
 #endif
